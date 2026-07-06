6ce178950c3d1658f40cd62a8ae3e7060ae8a1ab
brenobDEV <brenobraga411@gmail.com>
2026-07-06 20:42:18 -0300

    prog25.py

---
+++ prog25.py
@@ -0,0 +1,6 @@
+v1 = int(input("Digite aqui o seu primeiro valor - "))
+v2 = int(input("Digite aqui o seu segundo valor - "))
+if v1 > v2:
+    print(f"{v1} é o maior número")
+else: 
+    print(f"{v2} é o maior número")
