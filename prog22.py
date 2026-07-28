0c1002b49d86295f19525a98ecdac0c00da8d16f
git <brenobraga411@gmail.com>
2026-07-28 16:11:52 -0300

    atualizações

--- prog22.py
+++ prog22.py
@@ -1,6 +1,6 @@
 dia=input("digite o dia da semana ")
 match dia:
-    case "segunda" | "terça" | "quarta"| "quinta" | "sexta"
+    case "segunda" | "terça" | "quarta"| "quinta" | "sexta":
         print("Dia de semana. DIA DE PROGRAMAÇÃO")
     case "sábado" | "domingo":
         print("fim de semana! hora de descansar.")
